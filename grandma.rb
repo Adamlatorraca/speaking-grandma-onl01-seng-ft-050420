53235b3d0ff6b79a9726d4472d958b4102dc9c9e
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-04 23:11:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-  def speak_to_grandma(phrase)
+  def speak_to_grandma(phrase - "hi grandma")
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
       puts "HUH?! SPEAK UP, SONNY"
